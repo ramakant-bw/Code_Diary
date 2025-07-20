c90f28229e805f11d9cd04f7a9a6517af68df9ab
Ramakant Bawane <ramakant.bw@gmail.com>
2025-07-20 21:02:06 +0530

    2 questions solved on arstist table

--- sql_practice.sql
+++ sql_practice.sql
@@ -60,6 +60,24 @@ Inner Join Tracks as T On AL.AlbumID = T.AlbumID;
 
 -------------------------------------------------------------------------
 
+select * from Tracks
+
+-- Now we solve some questions based on this data 
+
+-- 1. Run Query: Find all the tracks that have a length of 2 minutes or more.
+  select a.name, t.minutes from Artists as a  inner join tracks as t on a.ArtistID = t.AlbumID  where t.minutes>= '00:02:00';
+
+
+-- 2. Run Query: Find all the tracks whose composer starts with 'Ed'.
+select name from Tracks where composer Like 'Ed%'
+
+
+-- 3. Find the albums with 5 or more tracks.
+
+select A.Title, Count(T.TrackID) from Albums as A inner join Tracks as T 
+on A.AlbumID = T.AlbumID Group by A.Title having Count(T.TrackID) >= 5 Order By Count(T.TrackID) Desc
+
+
 -------------------------------------------------------------------------
 
 -------------------------------------------------------------------------
