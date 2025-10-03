f0a3ac6b4d80b32af21cbe6bfb5ca4a2f564639e
Ramakant Bawane <ramakant.bw@gmail.com>
2025-10-04 00:49:01 +0530

    dataframe added

--- Python/panda.py
+++ Python/panda.py
@@ -6,3 +6,16 @@ ps1 =pd.Series(l1)
 
 print(ps1)
 print(type(ps1))
+
+
+data = {
+    'name':['amol','rahul','sham','ram'],
+    'age':[24,30,25,32],
+    'city':['mumbai','delhi','surat','nagpur']
+}
+
+df = pd.DataFrame(data)
+
+print(f'type of df is : {type(df)}')
+
+print(df)
